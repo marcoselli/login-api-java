797c9cf504077f6a6b86882fff486289fc8588fe
Marco <marco_sellI@@hotmail.com>
2022-01-10 17:04:34 -0300

    initial commit

---
+++ kyros-challenge/src/main/java/br/com/devmarco/kyroschallenge/ApiLoginApplication.java
@@ -0,0 +1,13 @@
+package br.com.devmarco.kyroschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLoginApplication.class, args);
+	}
+
+}
